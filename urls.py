a07dbd92bcadc4b59b606be5f585338060b2debe
Rodrigo Medina Muñoz <r_medina@inacap.cl>
2025-12-09 12:00:11 -0300

    Agregar ruta del mockup

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('platos/', include('platos.urls')),
+    path('mockup/', TemplateView.as_view(template_name='mockup/index.html'), name='mockup'),
+    path('', include('platos.urls')),  # Si tienes esto para la raíz
 ]
